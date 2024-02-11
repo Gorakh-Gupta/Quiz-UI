6a882b62f4fb71c50d4bb66221b53f5d9dc32758
Gorakh Gupat <gorakh.gupta23@gmail.com>
2024-02-12 01:03:35 +0530

    login page added

--- src/Router.js
+++ src/Router.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react'
 import { BrowserRouter as Router, Switch, Route, Routes } from 'react-router-dom'
 import { Navbar } from './components/Navbar'
 import { SignUp } from './components/SignUp'
+import { Login } from './components/Login'
 
 
 function RouterPath() {
@@ -9,12 +10,12 @@ function RouterPath() {
         <div>
             <Router>
                 <Routes>
-                    
-                    <Route path="/" exact element={<Navbar />} />
 
+                    <Route path="/" exact element={<Navbar />} />
                     //user
-                    <Route path="/signup"  element={<SignUp />} />
-                    
+                    <Route path="/signup" element={<SignUp />} />
+                    <Route path="/login" element={<Login />} />
+                
                 </Routes>
             </Router>
         </div>
