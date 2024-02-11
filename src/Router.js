17ae7e25d3a80b23a6ae02d20b2349f1aba3d6b3
Gorakh Gupat <gorakh.gupta23@gmail.com>
2024-02-11 15:24:50 +0530

    sign-up page added

--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,20 @@
 import React, { useContext } from 'react'
 import { BrowserRouter as Router, Switch, Route, Routes } from 'react-router-dom'
 import { Navbar } from './components/Navbar'
+import { SignUp } from './components/SignUp'
+
 
 function RouterPath() {
     return (
         <div>
             <Router>
                 <Routes>
+                    
                     <Route path="/" exact element={<Navbar />} />
+
+                    //user
+                    <Route path="/signup"  element={<SignUp />} />
+                    
                 </Routes>
             </Router>
         </div>
