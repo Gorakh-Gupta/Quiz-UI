108bf4a9a7e76c24e218a52ecd8198ca514d1656
Gorakh Gupat <gorakh.gupta23@gmail.com>
2024-02-14 18:13:25 +0530

    create quiz ui done

--- src/Router.js
+++ src/Router.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route, Routes } from 'react-router-dom
 import { Navbar } from './components/Navbar'
 import { SignUp } from './components/SignUp'
 import { Login } from './components/Login'
+import { Quiz } from './components/Quiz'
 
 
 function RouterPath() {
@@ -15,7 +16,11 @@ function RouterPath() {
                     //user
                     <Route path="/signup" element={<SignUp />} />
                     <Route path="/login" element={<Login />} />
-                
+               
+
+                    //quiz
+                    <Route path="/createquiz" exact element={<Quiz />} />
+                 
                 </Routes>
             </Router>
         </div>
